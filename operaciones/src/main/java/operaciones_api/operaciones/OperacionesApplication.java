ef2730a40550c49d90ba612e3e5967fc3eeda0e5
Salomorwil <salomorwil@gmail.com>
2025-05-21 17:03:50 -0400

    segundo commit, se generan las 5 apis, esta vez de verdad por desarrollar, administracion, logistica, operaciones, soporte y sucursal

---
+++ operaciones/src/main/java/operaciones_api/operaciones/OperacionesApplication.java
@@ -0,0 +1,13 @@
+package operaciones_api.operaciones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OperacionesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OperacionesApplication.class, args);
+	}
+
+}
---
+++ operaciones/src/test/java/operaciones_api/operaciones/OperacionesApplicationTests.java
@@ -0,0 +1,13 @@
+package operaciones_api.operaciones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OperacionesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
