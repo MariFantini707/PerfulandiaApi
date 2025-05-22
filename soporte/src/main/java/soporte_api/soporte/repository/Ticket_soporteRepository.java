212949fcc83bd1dc52c3a19da5542c80dc4edf48
Salomorwil <salomorwil@gmail.com>
2025-05-22 16:07:44 -0400

    Repository soporte

--- soporte/src/main/java/soporte_api/soporte/repository/Ticket_soporteRepository.java
+++ soporte/src/main/java/soporte_api/soporte/repository/Ticket_soporteRepository.java
@@ -1,9 +1,15 @@
 package soporte_api.soporte.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import soporte_api.soporte.model.Ticket_soporte;
 
 public interface Ticket_soporteRepository extends JpaRepository<Ticket_soporte, Integer> {
-
+    // Buscar por estado
+    List<Ticket_soporte> findByEstadoTicket(String estado_ticket);
+    
+    // Buscar por id
+    List<Ticket_soporte> findByIdTicket(String id_ticket);
 }
